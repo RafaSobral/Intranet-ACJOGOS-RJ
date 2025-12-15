bb977b261863f2a2b55513e4c7dfbd418a776368
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-12-14 22:54:56 -0300

    view de teste

--- pesquisas/views.py
+++ pesquisas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def responder_pesquisa_view(request, ano):
+    return HttpResponse(f"<h1>Pesquisa {ano}</h1><p>Em desenvolvimento...</p>")
+
+@login_required
+def minhas_pesquisas_view(request):
+    return HttpResponse("<h1>Minhas Pesquisas</h1><p>Em desenvolvimento...</p>")
