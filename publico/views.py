b1eed20db97c01650c440e2490d739e7ab1c2cec
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-12-14 22:45:17 -0300

    view de teste

--- publico/views.py
+++ publico/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.urls import path
+from django.http import HttpResponse
 
-# Create your views here.
+app_name = 'publico'
+
+# View temporária só para testar
+def home_view(request):
+    return HttpResponse("Bem-vindo à ACJOGOS-RJ!")
+
+urlpatterns = [
+    path('', home_view, name='home'),
+]
