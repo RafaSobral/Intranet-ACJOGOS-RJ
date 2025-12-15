c10506f26680767a408f0c5e2bd74dccdf335569
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-12-14 22:23:59 -0300

    registrando model usuario no admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    list_display = ['username', 'nome_completo', 'tipo_usuario', 'email', 'ativo']
+    list_filter = ['tipo_usuario', 'ativo', 'is_staff']
+    search_fields = ['username', 'nome_completo', 'email', 'cpf']
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informações Adicionais', {
+            'fields': ('tipo_usuario', 'nome_completo', 'nome_social', 'cpf', 
+                      'telefone', 'discord_nick')
+        }),
+        ('Endereço', {
+            'fields': ('cep', 'endereco', 'numero', 'complemento', 
+                      'bairro', 'cidade', 'estado')
+        }),
+        ('Status', {
+            'fields': ('ativo',)
+        }),
+    )
